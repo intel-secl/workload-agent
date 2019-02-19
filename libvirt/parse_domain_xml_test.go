e1176941b68b560a78183e36e3dbc6189f20e77a
HarshithaGowda <harshitha.m.gowda@intel.com>
2019-02-18 20:51:04 -0800

    Fixed parse_domain_xml_test.go test method

--- libvirt/parse_domain_xml_test.go
+++ libvirt/parse_domain_xml_test.go
@@ -10,32 +10,31 @@ import (
 
 func TestGetItemFromDomainXML(t *testing.T) {
 	domainXMLFile := "../test/domain.xml"
-	var parser *DomainParser
-	domainXMLFileContent, _ := ioutil.ReadFile(domainXMLFile)
+	// read domain XML file
+	domainXMLFileContent, err := ioutil.ReadFile(domainXMLFile)
+	assert.NoError(t, err)
+
+	// parse domain XML file
 	domainXML, err := xmlpath.Parse(bytes.NewReader(domainXMLFileContent))
 	assert.NoError(t, err)
 
-	parser = &DomainParser{
-		XML : domainXML,      
-		QemuInterceptCall : Start,
-	}
-	
-	parsedValue, err := NewDomainParser(parser)
+	// get all the values from domain XML
+	d, err := NewDomainParser(domainXML, Start)
 	assert.NoError(t, err)
+
 	// get vm UUID from domain XML
-	vmUUID := parsedValue.VMUUID
+	vmUUID := d.GetVMUUID()
 	assert.Equal(t, vmUUID, "412ea302-1759-440b-894a-bfef290d7a63")
 	// get vm path from domain XML
-	vmPath := parsedValue.VMPath
+	vmPath := d.GetVMPath()
 	assert.Equal(t, vmPath, "/var/lib/nova/instances/412ea302-1759-440b-894a-bfef290d7a63/disk")
 	// get image UUID from domain XML
-	imageUUID := parsedValue.ImageUUID
+	imageUUID := d.GetImageUUID()
 	assert.Equal(t, imageUUID, "31ab5921-24fd-498c-8c9e-b20f61004fc0")
 	// get image path from domain XML
-	imagePath := parsedValue.ImagePath
+	imagePath := d.GetImagePath()
 	assert.Equal(t, imagePath, "/var/lib/nova/instances/_base/dbee5739d526f9b742b8c7d4d829097965f4f718")
 	// get disk size from domain XML
-	size := parsedValue.Size
-
+	size := d.GetDiskSize()
 	assert.Equal(t, size, 1)
 }
