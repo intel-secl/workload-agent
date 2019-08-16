cb54b9f6c5106d468a6e6904c9ab66b12d1577f8
Hsiang-Yu <hsiang-yu.cheng@intel.com>
2019-08-16 14:21:22 -0700

    add rw lock

--- wlsclient/send_http_request.go
+++ wlsclient/send_http_request.go
@@ -3,6 +3,7 @@ package wlsclient
 import (
 	"io/ioutil"
 	"net/http"
+	"sync"
 
 	"intel/isecl/wlagent/config"
 	"intel/isecl/wlagent/consts"
@@ -12,22 +13,39 @@ import (
 )
 
 var aasClient = aas.NewJWTClient(config.Configuration.Aas.BaseURL)
+var aasRWLock = sync.RWMutex{}
 
-func addJWTToken(req *http.Request) error {
-
+func init() {
+	aasRWLock.Lock()
 	if aasClient.HTTPClient == nil {
 		c, err := clients.HTTPClientWithCADir(consts.TrustedCaCertsDir)
 		if err != nil {
-			return err
+			return
 		}
 		aasClient.HTTPClient = c
-		aasClient.AddUser(config.Configuration.Wls.APIUsername, config.Configuration.Wls.APIPassword)
-		aasClient.FetchAllTokens()
 	}
+	aasRWLock.Unlock()
+}
 
+func addJWTToken(req *http.Request) error {
+
+	aasRWLock.RLock()
 	jwtToken, err := aasClient.GetUserToken(config.Configuration.Wls.APIUsername)
+	aasRWLock.RUnlock()
+	// something wrong
 	if err != nil {
-		return err
+		// lock aas with w lock
+		aasRWLock.Lock()
+		// check if other thread fix it already
+		jwtToken, err = aasClient.GetUserToken(config.Configuration.Wls.APIUsername)
+		// it is not fixed
+		if err != nil {
+			// these operation cannot be done in init() because it is not sure
+			// if config.Configuration is loaded at that time
+			aasClient.AddUser(config.Configuration.Wls.APIUsername, config.Configuration.Wls.APIPassword)
+			aasClient.FetchAllTokens()
+		}
+		aasRWLock.Unlock()
 	}
 	req.Header.Set("Authorization", "Bearer "+string(jwtToken))
 	return nil
@@ -52,7 +70,9 @@ func SendRequest(req *http.Request, insecureConnection bool) ([]byte, error) {
 	defer response.Body.Close()
 	if response.StatusCode == http.StatusUnauthorized {
 		// fetch token and try again
+		aasRWLock.Lock()
 		aasClient.FetchAllTokens()
+		aasRWLock.Unlock()
 		err = addJWTToken(req)
 		if err != nil {
 			return nil, err
