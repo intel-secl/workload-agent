f4db7820afe334de798c81558e3c0e531adba843
R, Praveenraj <praveenraj.r@intel.com>
2020-03-04 23:52:23 -0800

    update script

--- dist/linux/deploy-to-artifactory.sh
+++ dist/linux/deploy-to-artifactory.sh
@@ -3,5 +3,5 @@ ArtifactId=workload-agent
 Version=2.0-SNAPSHOT
 GroupId=com.intel.isecl
 MavenRepoPath=/root/.m2/repository/
-cp out/${ArtifactId}-v1.*bin out/${ArtifactId}-${Version}.bin
+cp out/${ArtifactId}-v2.*bin out/${ArtifactId}-${Version}.bin
 mvn  install:install-file -Durl=file://${MavenRepoPath} -Dfile=out/${ArtifactId}-${Version}.bin -Dtype=bin -DartifactId=${ArtifactId} -DgroupId=${GroupId} -Dversion=${Version} -DpomFile=dist/linux/pom.xml -Dpackaging=bin
