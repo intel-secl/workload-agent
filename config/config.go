16253dfce7d768edeb12983fc4f6d1bb0880ce38
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-05-10 21:29:06 -0700

    small fix

--- config/config.go
+++ config/config.go
@@ -270,18 +270,26 @@ func SaveConfiguration(c csetup.Context, taskName string) error {
 func LogConfiguration(isStdOut bool) {
 	// creating the log file if not preset
 	var ioWriterDefault io.Writer
-	var err error = nil
-	secLogFile, _ := os.OpenFile(consts.SecurityLogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+
+	var err error
+	secLogFile, err := os.OpenFile(consts.SecurityLogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+	if err != nil {
+		log.Errorf("config/config:LogConfiguration() error in opening a security log file")
+	}
 	err = os.Chmod(consts.SecurityLogFile, 0640)
 	if err != nil {
 		log.Errorf("config/config:LogConfiguration() error in setting file permission for file : %s", secLogFile)
 	}
 
-	defaultLogFile, _ := os.OpenFile(consts.DefaultLogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+	defaultLogFile, err := os.OpenFile(consts.DefaultLogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+	if err != nil {
+		log.Errorf("config/config:LogConfiguration() error in opening a default log file")
+	}
 	err = os.Chmod(consts.DefaultLogFile, 0640)
 	if err != nil {
 		log.Errorf("config/config:LogConfiguration() error in setting file permission for file : %s", defaultLogFile)
 	}
+
 	ioWriterDefault = defaultLogFile
 
 	if isStdOut {
