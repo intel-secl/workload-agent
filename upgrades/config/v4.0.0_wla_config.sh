b81dc41eced52824f63dca0773f87ebeaf0c3070
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2021-06-28 15:58:06 +0530

    detect exit status

--- upgrades/config/v4.0.0_wla_config.sh
+++ upgrades/config/v4.0.0_wla_config.sh
@@ -5,4 +5,7 @@ BINARY_NAME=wlagent
 
 echo "Starting $COMPONENT_NAME config upgrade to v4.0.0"
 ./$BINARY_NAME setup all --force
+if [ $? != 0 ]; then
+  exit 1
+fi
 echo "Completed $COMPONENT_NAME config upgrade to v4.0.0"
