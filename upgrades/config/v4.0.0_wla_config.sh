2b09d8078a405381fec536f6ff3144420c2becac
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2021-06-28 15:41:25 +0530

    add v4 upgrade support

---
+++ upgrades/config/v4.0.0_wla_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPONENT_NAME=workload-agent
+BINARY_NAME=wlagent
+
+echo "Starting $COMPONENT_NAME config upgrade to v4.0.0"
+./$BINARY_NAME setup all --force
+echo "Completed $COMPONENT_NAME config upgrade to v4.0.0"
